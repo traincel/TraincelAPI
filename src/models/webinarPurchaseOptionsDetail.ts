b0d6c616490f82f596bf146be053a4a1ee6a9f5c
glassescode <glassescodes@gmail.com>
2021-03-26 23:26:12 +0530

    Fix model

--- src/models/webinarPurchaseOptionsDetail.ts
+++ src/models/webinarPurchaseOptionsDetail.ts
@@ -1,7 +1,9 @@
+import PurchaseOption from "./purchaseOption";
+
 export default interface WebinarPurchaseOptionsDetail{
     purchaseOptionId: number;
     maxCount: number;
     maxDuration: number;
     price: number;
-    purchaseOption: 
+    purchaseOption: PurchaseOption
 }
